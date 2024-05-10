ecc36ddea947a57e441bccba4e9b9feb054b1c8b
jab284 <jab284testing@yahoo.com>
2024-05-10 14:30:09 -0400

    Service Folder

--- obj/Debug/net8.0/JenAProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/JenAProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JenAProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d80b6d76969f9cdee1ff7814c4724e63f701849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74312a245613489f38c65902f059ba689e81b507")]
 [assembly: System.Reflection.AssemblyProductAttribute("JenAProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JenAProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
